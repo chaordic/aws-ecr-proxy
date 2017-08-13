14ce3802744a585c1fb746d7d721927fbd19563f
Marcelo Anton <marceloanton@gmail.com>
2017-08-13 03:58:32 -0300

    Not more use access key, try to get policies from instance role

--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -6,33 +6,28 @@ AWS_IAM='http://169.254.169.254/latest/dynamic/instance-identity/document'
 AWS_FOLDER='/root/.aws'
 
 region_config() {
-  echo  "region = $@" >> /root/.aws/config
+  echo "[default]" > ${AWS_FOLDER}/config
+  echo "region = $@" >> ${AWS_FOLDER}/config
 }
 
-if [[ "$AWS_KEY" != "" && "$AWS_SECRET" != "" ]]
-then
-  mkdir -p ${AWS_FOLDER}
-  echo "[default]
-aws_access_key_id = $AWS_KEY
-aws_secret_access_key = $AWS_SECRET" > ${AWS_FOLDER}/config
+mkdir -p ${AWS_FOLDER}
+chmod 700 ${AWS_FOLDER}
 
-	if [[ "$REGION" != "" ]]
-	then
-	  region_config $REGION
+if [[ "$REGION" != "" ]]; then
+    region_config $REGION
+
 # check if the region can be pulled from AWS IAM
-	elif wget -q -O- ${AWS_IAM} | grep -q 'region'
-	then
-		REGION=$(wget -q -O- ${AWS_IAM} | grep 'region'|cut -d'"' -f4)
-		region_config $REGION
-# error exit
-	else
-		echo "No region detected"
-		exit 1
-	fi
-# fix the permissions
-chmod 600 -R ${AWS_FOLDER}
+elif wget -q -O- ${AWS_IAM} | grep -q 'region'; then
+    REGION=$(wget -q -O- ${AWS_IAM} | grep 'region'|cut -d'"' -f4)
+    region_config $REGION
+
+else
+    echo "No region detected"
+    exit 1
 fi
 
+# fix the permissions
+chmod 600 -R ${AWS_FOLDER}/config
 
 # update the auth token
 aws_cli_exec=$(aws ecr get-login --no-include-email)
@@ -47,3 +42,4 @@ sed -i "s|REGISTRY_URL|$reg_url|g" ${nx_conf}
 /renew_token.sh &
 
 exec "$@"
+
